5f9007364a32e2eb7380c3897db5110f06085e06
Julian Roth <julian.roth1998@gmx.de>
2020-03-19 22:22:50 +0100

    Added some comments, logging and timeit.

--- Dataset/generateRandSparse.py
+++ Dataset/generateRandSparse.py
@@ -1,13 +1,14 @@
 import numpy as np
 import random
 import math
-from random import randint 
+from random import randint
 from scipy.sparse import dok_matrix , csc_matrix , identity , linalg
+from my_utils import timeit
+import logging
 
+logging.basicConfig(level=logging.DEBUG , format='[%(asctime)s] - [%(levelname)s] - %(message)s')
 
-
-
-
+# reference: https://en.wikipedia.org/wiki/Jacobi_rotation
 def jacobiRotation(dim):
     l = randint(0,dim-1)
     k = randint(0,dim-1)
@@ -15,7 +16,7 @@ def jacobiRotation(dim):
     theta = random.uniform(0,2*math.pi)
     c = math.cos(theta)
     s = math.sin(theta)
-    
+
     Q[k,k] = c
     Q[l,l] = c
     Q[k,l] = s
@@ -23,7 +24,10 @@ def jacobiRotation(dim):
 
     return Q.tocsc()
 
-class testSparse:
+# class for artificially created matrices
+# start with diagonal matrix with entries/ eigenvalues in certain range or list
+# then apply a few Jacobi Rotations until matrix is not too sparse anymore
+class CustomSparse:
     def __init__(self,dim,prob):
         self.dim = dim
         self.prob = prob
@@ -31,6 +35,7 @@ class testSparse:
         self.Q = identity(dim,dtype=np.float32,format = "csc")
         self.D = identity(dim,dtype=np.float32,format = "csc")
 
+    @timeit
     def create(self,eigenvalues,isList = False):
         diag = np.zeros(self.dim,dtype=np.float32)
         if isList:
@@ -40,27 +45,31 @@ class testSparse:
             a,b = eigenvalues
             for i in range(self.dim):
                 diag[i] = random.uniform(a,b)
-        
+
         self.D.setdiag(diag)
         self.A = self.D
-        
-    
+
+        # while matrix is too sparse, apply jacobi rotations
         while self.A.count_nonzero()/(float)(self.dim*self.dim) <= self.prob :
             tempQ = jacobiRotation(self.dim)
             self.A = tempQ.transpose() * self.A * tempQ
             self.Q = self.Q * tempQ
 
-    
+    @timeit
     def invert(self):
         diagonal = self.D.diagonal()
         tempD = csc_matrix(self.D)
-        tempD = linalg.inv(tempD)
-        
+
         for i in range(self.dim):
             tempD[i,i] = 1/diagonal[i]
+
         return self.Q.transpose() * tempD * self.Q
 
 
 
 
-
+if __name__ == "__main__":
+    # main function in python
+    custom = CustomSparse(10000,0.001)
+    custom.create([1,99999999])
+    print((custom.invert().dot(custom.A)-identity(custom.dim)).max())
