4ad2f4293345057e21caa7cf23f82241d7746059
mfschroeder <max.felix.schroeder@outlook.com>
2020-03-19 15:41:31 +0100

    few changes to the dataset

--- Dataset/generateRandSparse.py
+++ Dataset/generateRandSparse.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 import math
 from random import randint 
-from scipy.sparse import dok_matrix , csr_matrix
+from scipy.sparse import dok_matrix , csc_matrix , identity , linalg
 
 
 
@@ -11,51 +11,56 @@ from scipy.sparse import dok_matrix , csr_matrix
 def jacobiRotation(dim):
     l = randint(0,dim-1)
     k = randint(0,dim-1)
-    Q = dok_matrix((dim,dim), dtype=np.float32)
+    Q = identity(dim,dtype=np.float32,format = "dok")
     theta = random.uniform(0,2*math.pi)
     c = math.cos(theta)
     s = math.sin(theta)
-
-    for i in range(dim):
-        Q[i,i] = 1
     
     Q[k,k] = c
     Q[l,l] = c
     Q[k,l] = s
     Q[l,k] = -s
 
-    return Q.tocsr()
+    return Q.tocsc()
 
 class testSparse:
     def __init__(self,dim,prob):
         self.dim = dim
         self.prob = prob
-        self.A = csr_matrix((dim,dim),dtype=np.float32)
-        self.Q = csr_matrix((dim,dim),dtype=np.float32)
-        self.D = csr_matrix((dim,dim),dtype=np.float32)
+        self.A = csc_matrix((dim,dim),dtype=np.float32)
+        self.Q = identity(dim,dtype=np.float32,format = "csc")
+        self.D = identity(dim,dtype=np.float32,format = "csc")
 
-    def create(self,eigenvalues):
-        tempD = self.D.todok()
-        tempQ = self.Q.todok()
-        for i in range(self.dim):
-            tempD[i,i] = eigenvalues[i]
-            tempQ[i,i] = 1
-        self.D = tempD.tocsr()
-        self.Q = tempQ.tocsr()
+    def create(self,eigenvalues,isList = False):
+        diag = np.zeros(self.dim,dtype=np.float32)
+        if isList:
+            for i in range(self.dim):
+                diag[i] = eigenvalues[i]
+        else:
+            a,b = eigenvalues
+            for i in range(self.dim):
+                diag[i] = random.uniform(a,b)
+        
+        self.D.setdiag(diag)
         self.A = self.D
+        
     
         while self.A.count_nonzero()/(float)(self.dim*self.dim) <= self.prob :
             tempQ = jacobiRotation(self.dim)
             self.A = tempQ.transpose() * self.A * tempQ
             self.Q = self.Q * tempQ
 
+    
     def invert(self):
         diagonal = self.D.diagonal()
-        tempD = dok_matrix((self.dim,self.dim), dtype=np.float32)
+        tempD = csc_matrix(self.D)
+        tempD = linalg.inv(tempD)
+        
         for i in range(self.dim):
-            tempD[i,i] = diagonal[i]
-        return self.Q.transpose() * tempD.tocsr() * self.Q
+            tempD[i,i] = 1/diagonal[i]
+        return self.Q.transpose() * tempD * self.Q
 
-    
+
+
 
 
